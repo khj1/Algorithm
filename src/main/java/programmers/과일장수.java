e41d0fef6e9ca6774d9fb00224b5a1d3a3efaf86
khj1 <74335027+khj1@users.noreply.github.com>
2022-12-16 23:52:08 +0900

    programmers - [LV2] 과일장수, 점찍기

---
+++ src/main/java/programmers/과일장수.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/135808
+ */
+public class 과일장수 {
+
+	public int solution(int k, int m, int[] score) {
+		int answer = 0;
+		Integer[] boxedScore = boxScore(score);
+		Arrays.sort(boxedScore, Collections.reverseOrder());
+
+		int priceIndex = m - 1;
+		while (priceIndex < score.length) {
+			answer += m * boxedScore[priceIndex];
+			priceIndex += m;
+		}
+		return answer;
+	}
+
+	private static Integer[] boxScore(int[] score) {
+		return Arrays.stream(score)
+			.boxed()
+			.toArray(Integer[]::new);
+	}
+}
---
+++ src/main/java/programmers/점찍기.java
@@ -0,0 +1,18 @@
+package programmers;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/140107
+ */
+public class 점찍기 {
+
+	public long solution(int k, int d) {
+		long answer = 0;
+
+		for (int i = 0; i <= d / k; i++) {
+			long x = (long)i * k;
+			long y = (long)Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2)) / k;
+			answer += y + 1;
+		}
+		return answer;
+	}
+}
