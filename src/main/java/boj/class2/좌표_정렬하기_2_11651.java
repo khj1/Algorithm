2a5156bcbbb99994bb825591ff56dbf185ceeec0
khj1 <74335027+khj1@users.noreply.github.com>
2023-01-20 15:03:34 +0900

    boj - [CLASS2] 좌표 정렬하기 2

---
+++ src/main/java/boj/class2/좌표_정렬하기_2_11651.java
@@ -0,0 +1,55 @@
+package boj.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 좌표_정렬하기_2_11651 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		PriorityQueue<Point> points = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			points.add(new Point(x, y));
+		}
+
+		StringBuilder sb = new StringBuilder();
+		while (!points.isEmpty()) {
+			sb.append(points.poll()).append(System.lineSeparator());
+		}
+
+		System.out.println(sb);
+	}
+
+	static class Point implements Comparable<Point> {
+
+		int x;
+		int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			if (this.y == o.y) {
+				return this.x - o.x;
+			}
+			return this.y - o.y;
+		}
+
+		@Override
+		public String toString() {
+			return x + " " + y;
+		}
+	}
+
+}
